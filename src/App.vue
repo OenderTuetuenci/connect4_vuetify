3b734f8ae51bbb0ef869ebcc9a8839ffb3232176
OenderTuetuenci <oender.tuetuenci@gmx.de>
2021-01-12 15:41:58 +0100

    added pwa support

--- src/App.vue
+++ src/App.vue
@@ -60,5 +60,19 @@ export default {
       {title: 'Game', icon: 'mdi-play-circle',color:'white',to:'/'},
     ],
   }),
+  created() {
+    if (this.$workbox) {
+      this.$workbox.addEventListener("waiting", () => {
+        this.showUpgradeUI = true;
+      });
+    }
+  },
+  methods: {
+    async accept() {
+      this.showUpgradeUI = false
+      await this.$workbox.messageSW({ type: "SKIP_WAITING" });
+    }
+  },
 }
 </script>
+
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import wb from './registerServiceWorker';
 
 Vue.config.productionTip = false
+Vue.prototype.$workbox = wb;
 
 new Vue({
   router,
